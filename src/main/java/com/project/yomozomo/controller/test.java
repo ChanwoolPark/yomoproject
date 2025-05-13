fda09637ddddccc6bd86fffcdff7b06a19c20dc1
ChanwoolPark <wndqhr711@gmail.com>
2025-05-13 19:09:16 +0900

    테스트 바뀐 것

--- src/main/java/com/project/yomozomo/controller/test.java
+++ src/main/java/com/project/yomozomo/controller/test.java
@@ -2,4 +2,11 @@ package com.project.yomozomo.controller;
 
 public class test {
     //테슻트
+    // 이거를 수정해봐요
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
