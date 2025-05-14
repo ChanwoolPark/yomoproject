c3c1423633d0abdd84387d421021949d80686d6b
hyeeuney <zmasz@naver.com>
2025-05-14 17:15:44 +0900

    혜은이가 쓴 주석

---
+++ src/main/java/com/project/yomozomo/controller/HomeController.java
@@ -0,0 +1,54 @@
+package com.project.yomozomo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/category")
+    public String category() {
+        return "redirect:/category.html"; // 해당 html이 static에 존재할 경우
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "redirect:/login.html";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        // 세션 만료 등 처리
+        return "redirect:/";
+    }
+
+    @GetMapping("/my")
+    public String myPage() {
+        return "redirect:/my.html";
+    }
+
+    @GetMapping("/chatbot")
+    public String chatbot() {
+        return "redirect:/chatbot.html";
+    }
+
+    @GetMapping("/support")
+    public String support() {
+        return "redirect:/support.html";
+    }
+
+    @GetMapping("/explore")
+    public String explore() {
+        return "redirect:/explore.html";
+    }
+
+    @GetMapping("/ads")
+    public String ads() {
+        return "redirect:/ads.html";
+    }
+}
