8d3d3739cbf9d78eb3acb02a7f9a4e1b8ca3f6ee
yd42424 <soldyd424@gmail.com>
2025-05-19 16:00:11 +0900

    ㅇㅇㅇ

--- src/main/java/com/project/yomozomo/controller/HomeController.java
+++ src/main/java/com/project/yomozomo/controller/HomeController.java
@@ -52,6 +52,7 @@ public class HomeController {
     public String ads() {
         return "redirect:/ads.html";
     }
+
     @GetMapping("/charge")
     public String chargePage(Model model) {
         // 임시 예시: 실제로는 로그인 유저의 포인트를 DB에서 가져와야 함
