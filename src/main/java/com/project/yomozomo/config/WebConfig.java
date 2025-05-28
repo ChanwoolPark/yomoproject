f81dd8055cc3684c8a0b9aef2844ed08ff2854fd
hyeeuney <zmasz@naver.com>
2025-05-28 18:30:17 +0900

    config

--- src/main/java/com/project/yomozomo/config/WebConfig.java
+++ src/main/java/com/project/yomozomo/config/WebConfig.java
@@ -1,5 +1,5 @@
 // src/main/java/com/chat/config/WebConfig.java
-package com.project.yomozomo.Config;
+package com.project.yomozomo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
