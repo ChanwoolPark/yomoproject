022a0a7963bf87e24327440e11908f7b110bb71a
ChanwoolPark <wndqhr711@gmail.com>
2025-05-30 16:10:44 +0900

    05.30 16:10 : 사용자 다른글 목록 추가, 배너 링크 추가

--- src/main/java/com/project/yomozomo/repository/ProductRepository.java
+++ src/main/java/com/project/yomozomo/repository/ProductRepository.java
@@ -16,4 +16,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     // is_deleted = 'N' 조건 추가
     List<Product> findBySubCategory_Category_CategoryIdAndIsDeleted(int categoryId, String isDeleted);
     List<Product> findBySubCategory_SubCategoryIdAndIsDeleted(int subCategoryId, String isDeleted);
+
+    List<Product> findBySeller_IdAndIsDeletedAndProductIdNot(Long sellerId, String isDeleted, int excludeProductId);
 }
