bf90a558ce2a813707c8fca03d666d73d131e585
seungjuKim079 <seungjugim857@gmail.com>
2025-06-04 12:19:40 +0900

    Update Securityconfig.java

--- src/main/java/com/project/yomozomo/Config/Securityconfig.java
+++ src/main/java/com/project/yomozomo/Config/Securityconfig.java
@@ -1,5 +1,5 @@
 // src/main/java/com/project/yomozomo/config/SecurityConfig.java
-package com.project.yomozomo.Config;
+package com.project.yomozomo.config;
 
 import com.project.yomozomo.security.OAuth2LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
@@ -13,21 +13,27 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
-public class Securityconfig {
+public class SecurityConfig {
 
 
 
     private final OAuth2LoginSuccessHandler successHandler;
 
-    public Securityconfig(OAuth2LoginSuccessHandler successHandler) {
+    public SecurityConfig(OAuth2LoginSuccessHandler successHandler) {
         this.successHandler = successHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
