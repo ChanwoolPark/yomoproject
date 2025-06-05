6e167892fe1501cf670c04af972a9ec3efaf811b
hyeeuney <zmasz@naver.com>
2025-06-05 17:46:09 +0900

    푸쉬

--- src/main/java/com/project/yomozomo/config/SecurityConfig.java
+++ src/main/java/com/project/yomozomo/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
         System.out.println("★ SecurityConfig 로드됨");
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin1/**").hasAuthority("ADMIN")
+                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
                         .requestMatchers(
                                 "/", "/index.html", "/login", "/login/form", "/login/**",
                                 "/css/**", "/js/**", "/images/**",
