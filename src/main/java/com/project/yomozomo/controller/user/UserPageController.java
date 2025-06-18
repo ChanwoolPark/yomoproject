b585408a52096f96d849ee139f7a1d9ecf994432
hyeeuney <zmasz@naver.com>
2025-06-18 17:45:11 +0900

    1312123

--- src/main/java/com/project/yomozomo/controller/user/UserPageController.java
+++ src/main/java/com/project/yomozomo/controller/user/UserPageController.java
@@ -1,16 +1,26 @@
 package com.project.yomozomo.controller.user;
 
+import com.project.yomozomo.domain.Category;
+import com.project.yomozomo.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class UserPageController {
+    CategoryService categoryService;
+
     @GetMapping("/find-id")
     public String showFindIdPage() {
         return "find-id";
     }
     @GetMapping("/find-password")
     public String showFindPasswordPage() {
+
         return "find-password";
     }
 }
