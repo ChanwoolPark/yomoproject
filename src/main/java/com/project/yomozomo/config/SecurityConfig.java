05226872dd7edb9ba7b27eb25f54c99d03157c8d
yd42424 <soldyd424@gmail.com>
2025-06-19 12:30:17 +0900

    6/18

--- src/main/java/com/project/yomozomo/config/SecurityConfig.java
+++ src/main/java/com/project/yomozomo/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                                 "/charge", "/oauth2/**", "/signup", "/category",
                                 "/category/**","/api/**", "/find-id.html", "/find-password",
                                 "/find-id","/find-password.html",
-                                "/test-login"
+                                "/test-login","/search/**","product/**","/chatbot/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
